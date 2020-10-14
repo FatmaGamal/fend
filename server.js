74f9d5048716a3d6495b2c3079b825c6005a139c
Fatma Elnagar <fatma.elnagar@mondia.com>
2020-10-14 22:33:43 +0200

    journal / update data

---
+++ server.js
@@ -0,0 +1,54 @@
+// Setup empty JS object to act as endpoint for all routes
+let projectData = [];
+
+/* Dependencies */
+// Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+let port = 3000;
+// Callback to debug
+let listen = () => {
+    console.log(`server running`);
+    console.log(`listening on port ${port}`);
+};
+
+const server = app.listen(port, listen);
+
+// Callback function to complete GET '/all'
+const getData = (req, res) => {
+    //reply with DB object projectData
+    res.send(projectData[projectData.length - 1]);
+}
+
+const postData = (req, res) => {
+    // get object sent from client side
+    let data = req.body;
+    console.log(data);
+    // update DB
+    projectData.push(data);
+    res.send(projectData);
+}
+
+// Initialize all route with a callback function
+app.get('/getLatest', getData);
+
+// Post Route
+app.post('/update', postData);
+
---
+++ website/app.js
@@ -0,0 +1,87 @@
+/* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const PUBLIC_KEY = 'de09a960a4d4eca4b1898b5859b44859';
+let callUrl = `http://api.openweathermap.org/data/2.5/weather?zip=`;
+let publicKeyUrl = `&appid=${PUBLIC_KEY}`
+
+// Create a new date instance dynamically with JS
+function createDate() {
+    let d = new Date();
+    return newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+}
+
+function updateUI(data) {
+    // Updating Virtual DOM
+    const DOC_FRAGMENT = document.createDocumentFragment();
+    let entry = document.createElement('div');
+    entry.setAttribute('id', 'entryHolder');
+    let date = document.createElement('div');
+    date.setAttribute('id', 'date');
+    date.textContent = `Date: ${data.date}`;
+    let temp = document.createElement('div');
+    temp.setAttribute('id', 'temp');
+    temp.textContent = `Temperature: ${data.temp}`;
+    let content = document.createElement('div');
+    content.setAttribute('id', 'content');
+    content.textContent = `How you are feeling: ${data.response}`;
+
+    // Appending Virtual DOM to Actual DOM
+    entry.append(date);
+    entry.append(temp);
+    entry.append(content);
+    DOC_FRAGMENT.append(entry);
+    document.querySelector('.holder.entry').replaceChild(DOC_FRAGMENT, document.querySelector('#entryHolder'));
+}
+
+/* Function to POST data */
+const postData = async (url, data) => {
+    console.log('data to post', data);
+    const result = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'same-origin',
+        body: JSON.stringify(data)
+    });
+    try {
+        const res = await result.json();
+        console.log(res);
+
+    }
+    catch(error) {
+        console.log(`error: ${error}`);
+    }
+}
+
+/* Function to GET Project Data */
+const getData = async (url) => {
+    const result = await fetch(url);
+    try {
+        const res = await result.json();
+        console.log(res);
+        return res;
+    }
+    catch (error) {
+        console.log(`error: ${error}`)
+    }
+}
+
+
+// Event listener to add function to existing HTML DOM element
+let updateWeatherForecastBtn = document.getElementById('generate');
+updateWeatherForecastBtn.addEventListener('click', (e) => {
+    let currentFeeling = document.getElementById('feelings').value;
+    let zipCode = document.getElementById('zip').value;
+
+    // get current data from weatherAPI
+    getData(callUrl + zipCode + publicKeyUrl).then(function(data) {
+        // update DB of data array
+        let newData = {temp: data.main.temp, response: currentFeeling, date: createDate()}
+        postData('/update', newData).then(function(){
+            // update UI with updated DB
+            getData('/getLatest').then(function(data) {
+                updateUI(data);
+            });
+        });
+    });
+
+})
